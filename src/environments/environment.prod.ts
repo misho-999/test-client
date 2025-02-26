d8ce43d5dc4300444607ad83bb6c3797f43da3b7
misho-999 <misho_angelov@yahoo.com>
2025-02-26 09:41:55 +0200

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverLocation: 'http://93.127.214.32'
+  // serverLocation: 'http://93.127.214.32'
+  serverLocation: 'http://localhost:8081'
 };
